2d669603e84f29666e90da68c90ec692ef6d98ca
KozlovAleksandr <aleksandr.an.kozlov@gmail.com>
2023-04-22 04:24:38 +0300

    readme

--- src/api/apiRequest.ts
+++ src/api/apiRequest.ts
@@ -1,9 +1,9 @@
 import getCurrencyCode from '../utils/getCurrencyCode';
 
 const BASE_URL = 'https://v6.exchangerate-api.com/v6';
-const API_KEY = '273fa9914a77b57f349cb0eb';
+//const API_KEY = '273fa9914a77b57f349cb0eb';
 //const API_KEY = '01dfa2ba6d7d80762a98019c';
-//const API_KEY = 'ac8c421bc47d2b2cc5fe3ef6';
+const API_KEY = 'ac8c421bc47d2b2cc5fe3ef6';
 
 export const currencyAPI = {
   getCurrencyCodes: async () => {
